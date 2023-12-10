fd20ddd54903fcd7b6088a0b6f7138e7135c5236
Felippe Régis <felippesantos90@hotmail.com>
2023-12-10 05:23:16 -0300

    quarto comit

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import params from './src/params';
+import Fields from './src/components/Fields';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Gello WORLD!!</Text>
+      <Text style={styles.welcome}> iniciando o Mines </Text>
+      <Text style={styles.instructions}> Tamanho da grade
+        {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+        <Fields />
+        <Fields opened />
+        <Fields opened nearMines={1}/>
+        <Fields opened nearMines={2}/>
+        <Fields opened nearMines={3}/>
+        <Fields opened nearMines={4}/>
+        <Fields opened nearMines={5}/>
+        <Fields opened nearMines={6}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +29,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  welcome: {
+    fontSize:20,
+    textAlign: 'center',
+    margin: 10,
+  }
 });
